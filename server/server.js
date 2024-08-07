0632105389c3ccc87fb128d9daaf3a8240f288cd
kot197 <160315848+kot197@users.noreply.github.com>
2024-08-08 01:36:44 +0700

    should work

--- server/server.js
+++ server/server.js
@@ -1,12 +1,92 @@
 const express = require('express');
-const app = express();
-const http = require('http');
-const server = http.createServer(app);
+const { createServer } = require('node:http');
+const { Server } = require("socket.io");
+const sqlite3 = require('sqlite3');
+const { open } = require('sqlite');
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
-});
+async function main() {
+   // open the database file
+   const db = await open({
+    filename: 'chat.db',
+    driver: sqlite3.Database
+  });
 
-server.listen(3000, () => {
-  console.log('listening on *:3000');
-});
+  // create our 'messages' table (you can ignore the 'client_offset' column for now)
+  await db.exec(`
+    CREATE TABLE IF NOT EXISTS messages (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        client_offset TEXT UNIQUE,
+        content TEXT
+    );
+  `);
+
+  const app = express();
+  const server = createServer(app);
+  const io = new Server(server, {
+    connectionStateRecovery: {}
+  });
+
+  app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+  });
+  
+  io.on('connection', async (socket) => {
+    if (socket.recovered) {
+      // recovery was successful: socket.id, socket.rooms and socket.data were restored
+      console.log("recovered!");
+      console.log("socket.rooms:", socket.rooms);
+      console.log("socket.data:", socket.data);
+    } else {
+      // new or unrecoverable session
+    }
+  
+    console.log(`connect ${socket.id}`);
+  
+    socket.on('disconnect', (reason) => {
+      console.log(`disconnect ${socket.id} due to ${reason}`);
+    });
+  
+    socket.on('chat message', async (msg, clientOffset, callback) => {
+      let result;
+      try {
+        // store the message in the database
+        console.log(msg + " " + clientOffset);
+        result = await db.run('INSERT INTO messages (content, client_offset) VALUES (?, ?)', msg, clientOffset);
+      } catch (e) {
+        // TODO handle the failure
+        if (e.errno === 19 /* SQLITE_CONSTRAINT */ ) {
+          // the message was already inserted, so we notify the client
+          callback();
+        } else {
+          // nothing to do, just let the client retry
+        }
+        return;
+      }
+      console.log("before emit chat message");
+      // include the offset with the message
+      io.emit('chat message', msg, result.lastID);
+      // acknowledge the event
+      callback();
+    });
+
+    if (!socket.recovered) {
+      // if the connection state recovery was not successful
+      try {
+        await db.each('SELECT id, content FROM messages WHERE id > ?',
+          [socket.handshake.auth.serverOffset || 0],
+          (_err, row) => {
+            socket.emit('chat message', row.content, row.id);
+          }
+        )
+      } catch (e) {
+        // something went wrong
+      }
+    }
+  });
+  
+  server.listen(3000, () => {
+    console.log('listening on *:3000');
+  }); 
+}
+
+main();
